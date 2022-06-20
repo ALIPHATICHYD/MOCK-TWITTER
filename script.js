d15f8872fe947d399d13562d0d262b2b7d3993a0
Adegoke Fuad Ajibola <105937740+ALIPHATICHYD@users.noreply.github.com>
2022-06-20 23:00:15 +0100

    Update script.js

--- script.js
+++ script.js
@@ -29,4 +29,4 @@
     const passwordInput = prompt('Enter your password')
 
     signIn(usernameInput, passwordInput);
-    console.log(userValid('ALIPHA', 'eniola'))
+    console.log(userValid('ALIPHA', 'HYDRO'))
